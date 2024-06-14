cba93cef3e870a4b7e85a9c9babfce1868506dfb
codygeek01 <s474996633@gmail.com>
2024-06-14 16:06:29 +0530

    A freestanding rust binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern  "C" fn _start() -> ! {
+    loop {}
 }
